ed6148538ac8470323b980ffc799c37e67c20082
Waihon Yew <yewwaihon@gmail.com>
2021-04-04 21:48:57 +0800

    Initial Commit

---
+++ Bullseye/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bullseye
+//
+//  Created by Waihon Yew on 4/4/21.
+//  Copyright © 2021 Waihon Yew. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
