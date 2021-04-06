4eb7c686596d03771067882ea00a7a51934053d5
Waihon Yew <yewwaihon@gmail.com>
2021-04-06 22:58:26 +0800

    Style the text like the designer's design

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -12,6 +12,11 @@ struct ContentView: View {
   var body: some View {
     VStack {
       Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+        .bold()
+        .kerning(2.0)
+        .multilineTextAlignment(.center)
+        .lineSpacing(4.0)
+        .font(.footnote)
       Text("89")
       HStack {
         Text("1")
